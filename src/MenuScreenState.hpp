d3ad56f9a28f55a42ff585afa1b8d2fbe1d28710
Paul Mangold <nicogame69@yahoo.fr>
2015-08-22 12:53:09 +0200

    Reajout des fichiers partis.

---
+++ src/MenuScreenState.hpp
@@ -0,0 +1,29 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <vector>
+
+#include "ScreenState.hpp"
+
+class MenuScreenState : public ScreenState
+{
+public:
+    MenuScreenState();
+    virtual ~MenuScreenState()=default;
+
+    void event(const sf::RenderTarget& target, const sf::Event& event);
+    void render(sf::RenderTarget& target);
+    void update(const sf::Time& time);
+
+
+private:
+    int m_menuSize;
+    int m_selected;
+
+    sf::View m_view;
+
+    sf::Font m_font;
+    std::vector< sf::Text > m_menus;
+
+    sf::Texture m_background_texture;
+    sf::Sprite m_background;
+};
