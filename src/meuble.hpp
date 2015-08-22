3b9a0dfc7bec2dc733eb27e2586985dfa77a5be0
Thibault Dardinier <t.dardinier@gmail.com>
2015-08-22 17:02:15 +0200

    Ajouts de classes
    Meubles, niveaux…

--- src/meuble.hpp
+++ src/meuble.hpp
@@ -16,45 +16,52 @@ class Monster;
 class Lampe
 {
 public:
-    bool isLighting(sf::Vector2f point);
-    bool switcher();
+    bool isLighting(sf::Vector2f point) const;
+    void switcher();
 private:
     sf::Vector2f m_origin;
     float m_rayon;
-    bool m_isOn;
+    bool m_isOn; 
 };
-
 
 /* classe virtuelle pure du meuble de base, avec lequel on intéragit,
  * et les classes héritées des meubles plus spécifiques.
  * Les fonctions "interact" sont spécifiques pour chaques meubles.
  */
 
+enum Action {CACHER, ALLUMER, BRUITER, FINIR};
+
 class Meuble
 {
 public:
-    virtual bool canInteract(sf::Vector2f point);
-    virtual void interact(Monster &me);
+    virtual bool canInteract(sf::Vector2f point) const;
+    virtual Action interact(Monster &me);
     virtual ~Meuble();
 private:
     sf::Sprite const m_sprite;
     sf::FloatRect const m_hitBox;
 };
 
+class Lit: public Meuble
+{
+public:
+    virtual Action interact(Monster &me);
+    virtual ~Lit();
+};
+
 class Cachette: public Meuble
 {
 public:
-    virtual void interact(Monster &me);
+    virtual Action interact(Monster &me);
     virtual ~Cachette();
-private:
     bool m_used;
 };
 
 class Tapis: public Meuble
 {
 public:
-    virtual bool canInteract(sf::Vector2f point);
-    virtual void interact(Monster &me);
+    virtual bool canInteract(sf::Vector2f point) const;
+    virtual Action interact(Monster &me);
     virtual ~Tapis();
 private:
     sf::FloatRect const m_intHitBox;
@@ -64,7 +71,7 @@ private:
 class MeubleBruit: public Meuble
 {
 public:
-    virtual void interact(Monster &me);
+    virtual Action interact(Monster &me);
     virtual ~MeubleBruit();
 private:
     sf::Time m_temps;
@@ -73,10 +80,8 @@ private:
 class Interrupteur: public Meuble
 {
 public:
-    virtual void interact(Monster &me);
+    virtual Action interact(Monster &me);
     virtual ~Interrupteur();
 private:
     Lampe *m_light;
 };
-
-
