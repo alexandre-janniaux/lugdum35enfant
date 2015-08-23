88fdcd87895f59b56bcd441526467d1ffd0e969c
Novatux <nathanael.courant@laposte.net>
2015-08-23 16:19:04 +0200

    Triangle splitter should work

--- src/geometry.cpp
+++ src/geometry.cpp
@@ -1,9 +1,14 @@
 #include <SFML/Graphics.hpp>
+#include <vector>
 
 const float epsilon = 1e-3f;
 
 inline bool check(const float &x) {
 	return -epsilon <= x && x <= 1.f + epsilon;
+}
+
+inline bool check_strict(const float &x) {
+	return epsilon <= x && x <= 1.f - epsilon;
 }
 
 inline sf::Vector2f interp(const sf::Vector2f p1, const sf::Vector2f p2, float t) {
@@ -20,7 +25,7 @@ struct segment {
 		dir1 = p2 - p1;
 		dir2 = other.p2 - other.p1;
 		/*
-		Now we try to solve p1 + t1 * dir1 = p2 + t2 * dir2
+		Now we try to solve p1 + t1 * dir1 = other.p1 + t2 * dir2
 		This is a system of two linear equations
 		*/
 		float a = dir1.x;
@@ -31,8 +36,8 @@ struct segment {
 		if (-epsilon <= det && det <= epsilon) {
 			return sf::Vector2f(-42, -42); // Segments are parallel
 		}
-		float e = p2.x - p1.x;
-		float f = p2.y - p1.x;
+		float e = other.p1.x - p1.x;
+		float f = other.p1.y - p1.x;
 		float t1 = (d * e - b * f) / det;
 		float t2 = (-c * e + a * f) / det;
 		return sf::Vector2f(t1, t2);
@@ -45,19 +50,24 @@ struct segment {
 		}
 		return sf::Vector2f(-42, -42);
 	};
-	int intersection_triangle(const segment &tri1, const segment &tri2) {
-		it1 = intersection_time(tri1);
-		it2 = intersection_time(tri2);
-		it3 = intersection_time(segment(tri1.p2, tri2.p2));
-		segment seg1 = segment(tri1.p1, interp(tri1.p1, tri1.p2, it1.y));
-		segment seg2 = segment(tri2.p1, interp(tri2.p1, tri2.p2, it2.y));
-		segment seg3 = segment(tri1.p1, interp(tri1.p2, tri2.p2, it3.y));
-			
+	void intersection_triangle(const sf::Vector2f lumiere,
+			const segment &tri, std::vector<segment> &result) {
+		segment tri1 = segment(lumiere, tri.p1);
+		segment tri2 = segment(lumiere, tri.p2);
+		sf::Vector2f it1 = intersection_time(tri1);
+		sf::Vector2f it2 = intersection_time(tri2);
+		sf::Vector2f it3 = intersection_time(tri);
+		sf::Vector2f si1 = interp(lumiere, tri.p1, it1.y);
+		sf::Vector2f si2 = interp(lumiere, tri.p2, it2.y);
+		sf::Vector2f si3 = interp(tri.p1, tri.p2, it3.y);
+
 		if (!(check(it1.y) || check(it2.y) || check(it3.y))) {
 			// La droite du segment ne passe pas dans le triangle
-			return 0;
+			result.push_back(tri);
+			return;
 		}
-		if (!check(it3.y)) { 
+
+		if (!check_strict(it3.y)) { 
 			// La droite coupe les deux cotes principaux
 			float u, v;
 			sf::Vector2f pp1, pp2;
@@ -66,38 +76,107 @@ struct segment {
 			} else {
 				pp1 = p2; pp2 = p1; u = it2.x; v = it1.x;
 			}
-			//float u = it1.x;
-			//float v = it2.x;
-			if (v < -epsilon || u > 1.f + epsilon) {
+			if (v < epsilon || u > 1.f - epsilon) {
 				// Segment à l'extérieur du triangle
-				return 0;
+				result.push_back(tri);
+				return;
 			}
-			sf::Vector2f inter1 = segment(tri1.p2, tri2.p2).intersection(segment(tri1.p1, pp1));
-			sf::Vector2f inter2 = segment(tri1.p2, tri2.p2).intersection(segment(tri1.p1, pp2));
+			sf::Vector2f inter1 = tri.intersection(segment(lumiere, pp1));
+			sf::Vector2f inter2 = tri.intersection(segment(lumiere, pp2));
 			if (-epsilon <= u && v < 1.f + epsilon) {
-				// Completement a l'interieur	
-				// On découpe en:
-				// tri1, segment(tri1.p1, inter1)
-				// segment(tri1.p1, pp1), segment(tri1.p1, pp2)
-				// segment(tri1.p1, inter2), tri2
-				return 3;
+				// Completement a l'interieur
+				result.push_back(segment(tri.p1, inter1));
+				result.push_back(segment(pp1, pp2));
+				result.push_back(segment(inter2, tri.p2));
+				return;
 			}
 			// On intersecte un seul cote
 			if (check(it1.x)) {
 				// On intersecte cote 1
-				// On decoupe en:
-				// seg1, segment(tri1.p1, pp2);
-				// segment(tri1.p1, inter2), tri2
-				return 2;
+				result.push_back(segment(si1, pp2));
+				result.push_back(segment(inter2, tri.p2));
+				return;
 			} else {
 				// On intersecte cote 2
-				// On decoupe en:
-				// tri1, segment(tri1.p1, inter1)
-				// segment(tri1.p1, seg2)
-				return 2;
+				result.push_back(segment(tri.p1, inter1));
+				result.push_back(segment(pp1, si2));
+				return;
 			}
 		}
-		// TODO: Les autres possibilitees (si on touche le bout)
+
+		if (!check_strict(it1.y)) { 
+			// On coupe cote 2 et le bout
+			float u, v;
+			sf::Vector2f pp1, pp2;
+			if (it3.x < it2.x) {
+				pp1 = p1; pp2 = p2; u = it3.x; v = it2.x;
+			} else {
+				pp1 = p2; pp2 = p1; u = it2.x; v = it3.x;
+			}
+			if (v < epsilon || u > 1.f - epsilon) {
+				// Segment à l'extérieur du triangle
+				result.push_back(tri);
+				return;
+			}
+			sf::Vector2f inter1 = tri.intersection(segment(lumiere, pp1));
+			sf::Vector2f inter2 = tri.intersection(segment(lumiere, pp2));
+			if (-epsilon <= u && v < 1.f + epsilon) {
+				// Completement a l'interieur
+				result.push_back(segment(tri.p1, inter1));
+				result.push_back(segment(pp1, pp2));
+				result.push_back(segment(inter2, tri.p2));
+				return;
+			}
+			// On intersecte un seul cote
+			if (check(it3.x)) {
+				// On intersecte le bout
+				result.push_back(segment(tri.p1, si3));
+				result.push_back(segment(si3, pp2));
+				result.push_back(segment(inter2, tri.p2));
+				return;
+			} else {
+				// On intersecte cote 2
+				result.push_back(segment(tri.p1, inter1));
+				result.push_back(segment(pp1, si2));
+				return;
+			}
+		} else {
+			// On coupe cote 1 et le bout
+			float u, v;
+			sf::Vector2f pp1, pp2;
+			if (it1.x < it3.x) {
+				pp1 = p1; pp2 = p2; u = it1.x; v = it3.x;
+			} else {
+				pp1 = p2; pp2 = p1; u = it3.x; v = it1.x;
+			}
+			if (v < epsilon || u > 1.f - epsilon) {
+				// Segment à l'extérieur du triangle
+				result.push_back(tri);
+				return;
+			}
+			sf::Vector2f inter1 = tri.intersection(segment(lumiere, pp1));
+			sf::Vector2f inter2 = tri.intersection(segment(lumiere, pp2));
+			if (-epsilon <= u && v < 1.f + epsilon) {
+				// Completement a l'interieur
+				result.push_back(segment(tri.p1, inter1));
+				result.push_back(segment(pp1, pp2));
+				result.push_back(segment(inter2, tri.p2));
+				return;
+			}
+			// On intersecte un seul cote
+			if (check(it3.x)) {
+				// On intersecte le bout
+				result.push_back(segment(tri.p1, inter1));
+				result.push_back(segment(pp1, si3));
+				result.push_back(segment(si3, tri.p2));
+				return;
+			} else {
+				// On intersecte cote 1
+				result.push_back(segment(si1, pp2));
+				result.push_back(segment(inter2, tri.p2));
+				return;
+			}
+		}
 	};
 };
 
