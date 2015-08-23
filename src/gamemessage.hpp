07ba4885d36b9192cfe3e8bea004b1f34d188136
Alexandre Janniaux <alexandre.janniaux@gmail.com>
2015-08-23 17:45:21 +0200

    Add screen manipulation messages.

---
+++ src/gamemessage.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+struct ScreenMessage {
+	enum Action {Push, Pop, Clear}
+
+	static ScreenMessage Push(int id) {
+		action = Push;
+		screen = id;
+	}
+
+	static ScreenMessage Pop() {
+		action = Pop;
+		screen = -1;
+	}
+
+	static ScreenMessage Clear() {
+		action = Clear;
+		screen = -1;
+	}
+
+	int screen;
+	Action action;
+};
