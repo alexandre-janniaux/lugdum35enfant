70b9cc06259467c559797acec900884f240e58e0
NilpotentHX3 <antonin.riffard@free.fr>
2015-08-23 18:58:12 +0200

    Scene et sceneNode sont mieux maintenant (mais pas testés).

--- src/scene.cpp
+++ src/scene.cpp
@@ -7,13 +7,24 @@ Scene::Scene() {
 
 void Scene::draw(sf::RenderTarget& target,sf::RenderStates states) const
 {
-    std::multimap<int,SceneNode*> _renderQueue;
-    m_sceneNode->compute();
+    std::multimap<int,SceneNode const*> _renderQueue;
+    getRenderQueue(getRootNode(), _renderQueue);
+    m_sceneNode->getAbsoluteTransform();
+
 
     for (auto& it : _renderQueue)
-        target.draw(*it.second);
+        target.draw(*it.second, states);
 }
 
-SceneNode* Scene::getRootNode() {
-    return m_sceneNode.get();
+SceneNode& Scene::getRootNode() const
+{
+    return *m_sceneNode.get();
+}
+
+void getRenderQueue(SceneNode const& sn, std::multimap<int, SceneNode const*>& queue)
+{
+    queue.insert(std::pair<int,SceneNode const*>(sn.getLayer(), &sn));
+    std::vector<SceneNode*> const &children = sn.getChildren();
+    for (auto& it : children)
+        getRenderQueue(*it, queue);
 }
--- src/scene.hpp
+++ src/scene.hpp
@@ -9,10 +9,13 @@ class Scene : public sf::Drawable
 {
     public:
         Scene();
-        SceneNode* getRootNode();
+        SceneNode& getRootNode() const;
+
     private:
         virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
         std::unique_ptr<SceneNode> m_sceneNode;
 };
+
+void getRenderQueue(SceneNode const& sn, std::multimap<int, SceneNode const*>& queue);
 
 #endif // SCENE_HPP_INCLUDED
