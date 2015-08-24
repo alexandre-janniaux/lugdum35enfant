7fc602d0fcf9f205667d436e9f1ced0b1585be0b
Novatux <nathanael.courant@laposte.net>
2015-08-24 08:06:01 +0200

    Fix compilation

--- src/geometry.hpp
+++ src/geometry.hpp
@@ -25,7 +25,3 @@ struct Segment {
 	const sf::Vector2f intersection(const Segment &other) const;
 	void intersection_triangle(const sf::Vector2f lumiere, const Segment &tri, std::vector<Segment> &result);
 };
-<<<<<<< HEAD
-
-=======
->>>>>>> Debbuged geometry
