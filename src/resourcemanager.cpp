af798eeb4ee0983294b591d381e83214eb3b40db
Alexandre Janniaux <alexandre.janniaux@gmail.com>
2015-08-25 01:09:07 +0200

    Fix resourcemanager.

--- src/resourcemanager.cpp
+++ src/resourcemanager.cpp
@@ -1,8 +1,8 @@
 #include "resourcemanager.hpp"
 
 template <>
-std::unique_ptr<sf::Music>&& ResourceProvider(const std::string& filename) {
-	auto ptr = make_unique<sf::Music>();
+sf::Music* ResourceProvider(const std::string& filename) {
+	auto ptr = new sf::Music;
 	ptr->openFromFile(filename);
-	return std::move(ptr);
+	return ptr;
 }
