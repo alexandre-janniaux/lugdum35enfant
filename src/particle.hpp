4f00a512a7990cb27442d9657002617e6f21cfee
Frobenius42 <adrien.marcenat@orange.fr>
2015-08-24 13:19:45 +0200

    quelques modif de particle et test.text

--- src/particle.hpp
+++ src/particle.hpp
@@ -30,7 +30,6 @@ class ParticleEmitter : public SceneNode
 {
 public:
     ParticleEmitter(T copy, SceneNode& node, int);
-    //~ParticleEmitter();
     void setFrequency(float);
     void setNumberOfParticles(int);
     inline int getNumberOfParticles() { return m_particles.size(); }
@@ -38,38 +37,38 @@ public:
     void addParticle(int);
     void apply(Particle<T>*);
     void effect(Particle<T>*);
+    void setEmission(bool);
 private:
     std::vector<Particle<T>*> m_particles;
     float m_Frequency;
     int m_NumberOfParticles;
     SceneNode& m_node;
+    bool m_activate;
 };
 
 template<typename T>
-ParticleEmitter<T>::ParticleEmitter(T copy, SceneNode& node, int number)
+ParticleEmitter<T>::ParticleEmitter(T copy, SceneNode& snode, int number)
 : m_Frequency()
 , m_NumberOfParticles(number)
 , m_particles()
-, m_node(node)
+, m_activate(true)
+, m_node(snode)
 {
     setPosition({400.f,300.f});
     for (int i=0; i<m_NumberOfParticles; i++)
     {
         float angle(rand());
-        float life(0.5+(rand()%100)/100.f);
+        float life(0.1+(rand()%100)/1000.f);
         m_particles.push_back(new Particle<T> {getPosition(), (40.f+rand()%100)*(sf::Vector2f (cos(angle),sin(angle))), life, life, T() });
-        m_particles.back()->node.attachParent(&m_node);
+        m_particles.back()->node.attachParent(m_node);
     }
 }
 
-/*ParticleSystem::~ParticleSystem()
+template<typename T>
+void ParticleEmitter<T>::setEmission(bool activate)
 {
-    for( int i = m_particles.begin(); i != m_particles.end(); i++ )
-    {
-        delete m_particles[i];
-    }
+        m_activate = activate;
 }
-*/
 
 template<typename T>
 void ParticleEmitter<T>::setFrequency(float frequency)
@@ -86,23 +85,24 @@ void ParticleEmitter<T>::setNumberOfParticles(int number)
 template<typename T>
 void ParticleEmitter<T>::update(sf::Time time)
 {
-    auto t = getAbsoluteTransform();
-    auto tt = getTransform();
-    for (int i=0;i<m_particles.size(); i++)
+    if (m_activate)
     {
-        m_particles[i]->lifetime-=time.asSeconds();
-        if (m_particles[i]->lifetime<0)
+        for (int i=0;i<m_particles.size(); i++)
         {
-            delete m_particles[i];
-            m_particles[i] = nullptr;
-            addParticle(i);
-        }
-        else
-        {
-            m_particles[i]->pos+=m_particles[i]->vel*time.asSeconds();
-            m_particles[i]->node.setPosition(m_particles[i]->pos);
-            apply(m_particles[i]);
-            effect(m_particles[i]);
+            m_particles[i]->lifetime-=time.asSeconds();
+            if (m_particles[i]->lifetime<0)
+            {
+                delete m_particles[i];
+                m_particles[i] = nullptr;
+                addParticle(i);
+            }
+            else
+            {
+                m_particles[i]->pos+=m_particles[i]->vel*time.asSeconds();
+                m_particles[i]->node.setPosition(m_particles[i]->pos);
+                apply(m_particles[i]);
+                effect(m_particles[i]);
+            }
         }
     }
 }
@@ -111,8 +111,8 @@ void ParticleEmitter<T>::update(sf::Time time)
 template<>
 void ParticleEmitter<SmokeShape>::apply(Particle<SmokeShape>* particle)
 {
-    float gravity(particle->lifetime/particle->lifetime_max);
-    particle->vel*=gravity;
+    sf::Vector2f gravity(0.f,particle->lifetime/particle->lifetime_max);
+    particle->vel+=20.f*gravity;
 }
 
 template<>
@@ -132,15 +132,12 @@ template<typename T>
 void ParticleEmitter<T>::addParticle(int num)
 {
     float angle(rand());
-    float life(1.f+(rand()%100)/100.f);
+    float life(0.1+(rand()%100)/1000.f);
     m_particles[num] = new Particle<T> {getPosition(), (40.f+rand()%100)*(sf::Vector2f (cos(angle),sin(angle))), life, life, T() };
-    m_particles[num]->node.attachParent(&m_node);
+    m_particles[num]->node.attachParent(m_node);
 }
 
 template<typename T>
 void apply(Particle<T>* particle)
 {
 }
-
-
-
