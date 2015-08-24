8e58bde949d566814ea9d851865f055430562f4b
Alexandre Janniaux <alexandre.janniaux@gmail.com>
2015-08-24 23:13:04 +0200

    Fix typo.

--- src/resourcemanager.hpp
+++ src/resourcemanager.hpp
@@ -15,42 +15,45 @@
 template <typename T>
 class ResourceManager
 {
-public:
-    static ResourceManager* instance()
-    {
-	if(m_instance == nullptr)
-	{
-	    m_instance.reset(new ResourceManager<T>());
-	}
-	return m_instance.get();
-    }
-    
-    T& get(const std::string& nom_fichier)
-    {
-        std::string filename = nom_fichier;
-#if __APPLE__
-        filename = resourcePath() + nom_fichier;
-#endif
+	public:
+		static ResourceManager* instance()
+		{
+			if(m_instance == nullptr)
+			{
+				m_instance.reset(new ResourceManager<T>());
+			}
+			return m_instance.get();
+		}
+
+		T& get(const std::string& nom_fichier);
+
+		~ResourceManager() = default;
+	private:
+		ResourceManager() = default;
+		using Ptr = std::unique_ptr<ResourceManager<T>>;
+
+	    static Ptr m_instance;
+
+    	std::map<std::string, T*> m_resources;
+};
+
+template <typename T>
+T& ResourceManager<T>::get(const std::string& nom_fichier)
+{
+	std::string filename = nom_fichier;
+	#if __APPLE__
+	filename = resourcePath() + nom_fichier;
+	#endif
 
 	if(m_resources.find(filename) == m_resources.end())
 	{
-	    std::cout << "chargement de " << filename << std::endl;
-	    m_resources.insert(std::pair<std::string, T*>(filename, new T));
-	    m_resources[filename]->loadFromFile(filename);
+		std::cout << "chargement de " << filename << std::endl;
+		m_resources.insert(std::pair<std::string, T*>(filename, new T));
+		m_resources[filename]->loadFromFile(filename);
 	}
 	return *m_resources.at(filename);
-    }
-    
-    ~ResourceManager() { std::cout << "Delete loader" << std::endl; }
-private:
-    ResourceManager() { std::cout << "Create loader" << std::endl; }
-    using Ptr = std::unique_ptr<ResourceManager<T>>;
-    
-    //friend make_unique<ResourceManager<T>>();
-    
-    static Ptr m_instance;
-    std::map<std::string, T*> m_resources;
-};
+}
+
 
 template <typename T>
 typename ResourceManager<T>::Ptr ResourceManager<T>::m_instance = nullptr;
